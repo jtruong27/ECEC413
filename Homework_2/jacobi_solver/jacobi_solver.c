026582da4dce50fb8a640ceea39a19ebaff7a22a
mp999 <mp999@xunil-03.coe.drexel.edu>
2020-05-06 19:20:44 -0400

    resetting code

--- Homework_2/jacobi_solver/jacobi_solver.c
+++ Homework_2/jacobi_solver/jacobi_solver.c
@@ -1,11 +1,8 @@
 /* Code for the Jacobi method of solving a system of linear equations 
  * by iteration.
-
+ *
  * Author: Naga Kandasamy
  * Date modified: April 22, 2020
- *
- * Members: Minjae Park, John Truong
- * Modified: May 06, 2020
  *
  * Compile as follows:
  * gcc -o jacobi_solver jacobi_solver.c compute_gold.c -Wall -O3 -lpthread -lm 
@@ -16,24 +13,10 @@
 #include <time.h>
 #include <string.h>
 #include <math.h>
-#include <pthread.h>
 #include "jacobi_solver.h"
 
 /* Uncomment the line below to spit out debug information */ 
-#define DEBUG
-
-/* declaring Pthread Barriers for Sync */
-pthread_barrier_t barr;
-/* declaring semaphore for protected writing */
-typedef struct critical_section_s {
-  pthread_mutex_t mutex;
-  double value;
-} critical_section_t;
-
-critical_section_t ssd;
-/* global exit flag */
-int done;
-int num_iter = 0;
+/* #define DEBUG */
 
 int main(int argc, char **argv) 
 {
@@ -70,7 +53,8 @@ int main(int argc, char **argv)
   print_matrix(reference_x);
 #endif
 
-  /* Compute Jacobi solution using reference code */
+  /* Compute Jacobi solution
+   * using reference code */
   fprintf(stderr, "Generating solution using reference code\n");
   int max_iter = 100000; /* Maximum number of iterations to run */
   compute_gold(A, reference_x, B, max_iter);
@@ -82,18 +66,22 @@ int main(int argc, char **argv)
   print_matrix(reference_x);
 #endif
 
-  /* Compute the Jacobi solution using pthreads. 
-   * Solutions are returned in mt_solution_x.
-   * */
+  /* Compute the
+   * Jacobi
+   * solution
+   * using
+   * pthreads. 
+   *      *
+   *      Solutions
+   *      are
+   *      returned
+   *      in
+   *      mt_solution_x.
+   *           *
+   *           */
   fprintf(stderr, "\nPerforming Jacobi iteration using pthreads\n");
   compute_using_pthreads(A, mt_solution_x, B);
   display_jacobi_solution(A, mt_solution_x, B); /* Display statistics */
-
-#ifdef DEBUG
-  print_matrix(A);
-  print_matrix(B);
-  print_matrix(mt_solution_x);
-#endif
 
   free(A.elements); 
   free(B.elements); 
@@ -103,143 +91,17 @@ int main(int argc, char **argv)
   exit(EXIT_SUCCESS);
 }
 
-/* FIXME: Complete this function to perform the Jacobi calculation using pthreads. 
- * Result must be placed in mt_sol_x. */
+/* FIXME: Complete this function to perform the Jacobi calculation using
+ * pthreads. 
+ *  * Result must be placed in mt_sol_x. */
 void compute_using_pthreads (const matrix_t A, matrix_t mt_sol_x, const matrix_t B)
 {
-  thread_args_t *thread_args = (thread_args_t *) malloc(sizeof(thread_args_t));
-
-  /* data structure to store the threads */
-  pthread_t *worker_thread = (pthread_t *) malloc(NUM_THREADS * sizeof(pthread_t));
-  /* Barrier initialized */
-  pthread_barrier_init(&barr, 0, NUM_THREADS);
-  /* lock for global variable ssd */
-  pthread_mutex_init(&ssd.mutex, NULL); /* initialize the mutex */
-
-  int i;
-
-  /* initialize global variables */
-  done = 0;
-  ssd.value = 0.0;
-
-  for(i=0; i < NUM_THREADS; i++){
-    thread_args->tid = i;
-    thread_args->A = &A;
-    thread_args->x = &mt_sol_x;
-    thread_args->B = &B;
-    /* Failure to Create a worker thread and exits program */
-    /* Creates independent threads each of which will execute the function */
-    /* Start new thread in calling process and wait till
-     * threads are complete before guass function continues. */
-    /* If wait, run the risk of executing an exit which will
-     * terminate the process and all threads before completing */
-    if ((pthread_create (&worker_thread[i], NULL, jacobi_thread, (void *) thread_args)) != 0) {
-      printf("\nFailed to Create a Worker Thread\n");
-      exit(EXIT_FAILURE);
-    }
-  }
-
-  for (i = 0; i < NUM_THREADS; i++)
-  {
-    pthread_join(worker_thread[i], NULL);
-  }
-
-  pthread_barrier_destroy(&barr);
-  pthread_mutex_destroy(&ssd.mutex);
-  free((void *)worker_thread);
-  free((void *)thread_args);
-
-  return;
-}
-
-void *jacobi_thread(void *args){
-  /* declare variables */
-  matrix_t *A, *x, *B;
-  int tid, r, c;
-  /* loop variables */
-  int i, j;
-  /* barrier return value */
-  int barr_ret;
-
-  /* unpack arguments */
-  thread_args_t *arguments = (thread_args_t *)args;
-  tid = arguments->tid;
-  A = arguments->A;
-  B = arguments->B;
-  x = arguments->x;
-  r = A->num_rows;
-  c = A->num_columns;
-
-#ifdef DEBUG
-  print_matrix(*A);
-  print_matrix(*B);
-  print_matrix(*x);
-#endif
-
-  /* Allocate n x 1 matrix to hold iteration values */
-  matrix_t new_x = allocate_matrix(r, 1, 0);
-
-  /* Initialize current jacobi solution */
-  for (i = 0; i < r; i++)
-    x->elements[i] = B->elements[i];
-
-  /* perform jacobi iteration */
-  double partial_ssd, sum;
-
-  while(!done){
-    /* reset accumulator */
-    partial_ssd = 0.0;
-
-    for (i = tid; i < r; i += NUM_THREADS){
-      sum = -A->elements[i * c + i] * x->elements[i];
-      for (j = 0; j < c; j++)
-        sum += A->elements[i * c + j] * x->elements[i];
-
-      /* Update values for the unknowns for the current row */
-      new_x.elements[i] = (B->elements[i] - sum)/A->elements[i * c + i];
-
-      /* add to convergence value */
-      partial_ssd += (new_x.elements[i] - x->elements[i]) * (new_x.elements[i] - x->elements[i]);
-    }
-
-    /* update global convergence value */
-    pthread_mutex_lock(&ssd.mutex);
-    ssd.value += partial_ssd;
-    pthread_mutex_unlock(&ssd.mutex);
-
-    /* barrier synchronization */
-    barr_ret = pthread_barrier_wait(&barr);
-
-    /* check for convergence and update the unknowns */
-    if(barr_ret == PTHREAD_BARRIER_SERIAL_THREAD) {
-      num_iter++;
-      double mse = sqrt(ssd.value);
-
-      /* reset ssd */
-      pthread_mutex_lock(&ssd.mutex);
-      ssd.value = 0.0;
-      pthread_mutex_unlock(&ssd.mutex);
-
-      fprintf(stderr, "TID: %d. Iteration: %d. MSE = %f\n", tid, num_iter, mse);
-      if (mse <= THRESHOLD)
-        done = 1;
-    }
-
-    pthread_barrier_wait(&barr);
-
-    /* update x */
-    for(i = tid; i < r; i += NUM_THREADS)
-      x->elements[i] = new_x.elements[i];
-
-  }
-
-  pthread_exit(NULL);
 }
 
 /* Allocate a matrix of dimensions height * width.
-   If init == 0, initialize to all zeroes.  
-   If init == 1, perform random initialization.
-   */
+ *    If init == 0, initialize to all zeroes.  
+ *       If init == 1, perform random initialization.
+ *       */
 matrix_t allocate_matrix(int num_rows, int num_columns, int init)
 {
   int i;    
@@ -257,7 +119,7 @@ matrix_t allocate_matrix(int num_rows, int num_columns, int init)
   }
 
   return M;
-}	
+} 
 
 /* Print matrix to screen */
 void print_matrix(const matrix_t M)
@@ -317,10 +179,11 @@ matrix_t create_diagonally_dominant_matrix (int num_rows, int num_columns)
   for (i = 0; i < size; i++)
     M.elements[i] = get_random_number(MIN_NUMBER, MAX_NUMBER);
 
-  /* Make diagonal entries large with respect to the entries on each row. */
+  /* Make diagonal entries large with respect to the
+   * entries on each row. */
   float row_sum;
   for (i = 0; i < num_rows; i++) {
-    row_sum = 0.0;		
+    row_sum = 0.0;    
     for (j = 0; j < num_columns; j++) {
       row_sum += fabs(M.elements[i * M.num_rows + j]);
     }
@@ -336,6 +199,4 @@ matrix_t create_diagonally_dominant_matrix (int num_rows, int num_columns)
 
   return M;
 }
-
-
 
--- Homework_2/jacobi_solver/jacobi_solver.h
+++ Homework_2/jacobi_solver/jacobi_solver.h
@@ -4,7 +4,6 @@
 #define THRESHOLD 1e-5      /* Threshold for convergence */
 #define MIN_NUMBER 2        /* Min number in the A and b matrices */
 #define MAX_NUMBER 10       /* Max number in the A and b matrices */
-#define NUM_THREADS 1       /* number of threads */
 
 /* Matrix structure declaration */
 typedef struct matrix_s {
@@ -13,14 +12,6 @@ typedef struct matrix_s {
   float *elements;
 }  matrix_t;
 
-/* Thread argument structure declaration */
-typedef struct thread_args_s {
-  int tid;                    /* thread id */
-  const matrix_t *A;                /* coefficient matrix */
-  matrix_t *x;                /* unknowns matrix */
-  const matrix_t *B;                /* constants matrix */
-}  thread_args_t; 
-
 /* Function prototypes */
 matrix_t allocate_matrix (int, int, int);
 extern void compute_gold(const matrix_t, matrix_t, const matrix_t, int);
@@ -28,9 +19,9 @@ extern void display_jacobi_solution(const matrix_t, const matrix_t, const matrix
 int check_if_diagonal_dominant(const matrix_t);
 matrix_t create_diagonally_dominant_matrix(int, int);
 void compute_using_pthreads(const matrix_t, matrix_t, const matrix_t);
-void *jacobi_thread(void *);
 void print_matrix(const matrix_t);
 float get_random_number(int, int);
 
 #endif /* _JACOBI_SOLVER_H_ */
+
 
