3a1f3ffca3e6bda693087cfae0797edee2545c50
JTRUONG <51338213+jtruong27@users.noreply.github.com>
2020-06-03 13:23:49 -0400

    i hate life

--- Homework_5/CountingSort_CUDA/counting_sort.h
+++ Homework_5/CountingSort_CUDA/counting_sort.h
@@ -1,28 +1,9 @@
 #ifndef _COUNTING_SORT_H_
 #define _COUNTING_SORT_H_
 
-#include <stdlib.h>
-#include <stdio.h>
-#include <time.h>
-#include <sys/time.h>
-#include <string.h>
-#include <math.h>
-#include <limits.h>
-#include "counting_sort.h"
-#include "counting_sort_kernel.cu"
-
 #define HISTOGRAM_SIZE 256      /* initialize historgram size */
 
 #define RANGE 255               /* input array will have integer elements ranging from 0 to 255 range */
 #define NUM_ELEMENTS 100000000  /* Number of input integers */
-
-extern "C" int counting_sort_gold(int *, int *, int, int);
-int rand_int(int, int);
-void print_array(int *, int);
-void print_min_and_max_in_array(int *, int);
-void compute_on_device(int *, int *, int, int);
-int check_if_sorted(int *, int);
-int compare_results(int *, int *, int);
-void check_for_error(const char *);
 
 #endif
